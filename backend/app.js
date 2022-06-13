400f5fa69e97b2ed72f9e2337b7a25ab9e36ef5d
ArpitLinux <arpitytlinux@gmail.com>
2022-06-13 15:42:58 +0530

    First Commit

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import axios from "axios"
+
+import {SHAZAMOPTIONS, GOOGLEAPIKEY, GOOGLEAPICX} from "./keys.js"
+
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true}))
+
+app.post("/base64file", async (req, res, next) => {
+    console.log(req.headers['content-length'])
+    // SHAZAMOPTIONS.data = req.body.base64;
+
+    // await axios.request(SHAZAMOPTIONS).then(async function (response) {
+    //     var fullResponse = response.data
+    //     console.log(fullResponse)
+    //     var songName = response.data.track.share.subject
+    //     const GOOGLEOPTIONS = {
+    //         method: "GET",
+    //         url: `https://customsearch.googleapis.com/customsearch/v1?q=${songName}&key=${GOOGLEAPIKEY}&cx=${GOOGLEAPICX}`,
+    //     }
+    //     await axios.request(GOOGLEOPTIONS).then(function(anotherResponse) {
+    //         var obj = {"Song": songName, "Data": anotherResponse}
+    //     })
+    // }).catch(function (error) {
+    //         console.error(error);
+    //     });
+    
+})
+
+
+
+const port = 3000;
+
+app.listen(port, (error) => {
+        console.log("Server is Successfully Running and App is listening on port "+ port)
+});
